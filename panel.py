b3215c5af79948e606a1491d1660bd2774a8720a
Victor Moura <victor_cmoura@hotmail.com>
2019-11-01 11:13:51 -0300

    Adds thread to update data

--- panel.py
+++ panel.py
@@ -2,8 +2,21 @@ import pygame
 import requests
 import time
 import os
+import threading
+
+gtflights = None
+gflights = gtflights
+
+not_fetched = True
+should_fetch = True
+fetching = False
 
 def fetch_flights():
+    global should_fetch
+    global fetching
+    fetching = True
+    should_fetch = False
+    print("Fetching flights from API...")
     # url = 'https://opensky-network.org/api/states/all?lamin=-24.100386&lomin=-47.451401&lamax=-23.183942&lomax=-45.691472'
     url = 'https://opensky-network.org/api/states/all?lamin=-24.493696&lomin=-48.177301&lamax=-22.797153&lomax=-45.566508'
     r = requests.get(url)
@@ -21,7 +34,15 @@ def fetch_flights():
             'y': int(((each[6] + 24.100386) * 720)/y_factor)
         })
 
-    return flights
+    print("Flights fetched!")
+
+    global gtflights
+    gtflights = flights
+
+    global not_fetched
+    not_fetched = False
+
+    fetching = False
 
 def fetch_closest_points(flights):
     in_screen = []
@@ -68,32 +89,67 @@ font = pygame.font.SysFont("comicsansms", 20)
 mapim = pygame.image.load("spmap.png")
 mapim.convert()
 
+start_ticks=pygame.time.get_ticks()
+
+reload_time = 15
+
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
         
-        flights = fetch_flights()
+        seconds=(pygame.time.get_ticks()-start_ticks)/1000
+
+        curr_time = max(reload_time - seconds, 0)
 
         screen.blit(mapim, (0, 0))
 
-        dist_obj = fetch_closest_points(flights)
+        if not_fetched:
+            if not fetching:
+                should_fetch = True
 
-        for flight in flights:
-            text = font.render(flight['id'], True, (255, 0, 0))
-            screen.blit(text, (flight['x'], flight['y'] + text.get_height() + 3))
-            pygame.draw.circle(screen, (255, 0, 0), (flight['x'], flight['y']), 7)
+        if curr_time <= 0:
+            start_ticks=pygame.time.get_ticks()
+            gflights = gtflights
+            not_fetched = True
 
-        if dist_obj['distance'] <= 1000:
-            text = font.render(str(dist_obj['distance']), True, (0, 0, 0))
+        if should_fetch and not_fetched:
+            fb_thread = threading.Thread(target=fetch_flights)
+            fb_thread.start()
+        
+        if gflights == None and gtflights != None:
+            gflights = gtflights
+            not_fetched = True
 
-            text_pos = (
-                (dist_obj['p1'][0] + dist_obj['p2'][0])//2 - text.get_width() + 10,
-                (dist_obj['p1'][1] + dist_obj['p2'][1])//2 - text.get_height() - 10
-            )
+        if gflights != None:
+            dist_obj = fetch_closest_points(gflights)
 
-            pygame.draw.line(screen, (0, 0, 255), dist_obj['p1'], dist_obj['p2'], 3)
-            pygame.draw.rect(screen, (128, 128, 128), pygame.Rect(text_pos[0] - 5, text_pos[1] - 5, text.get_width() + 10, text.get_height() + 10))
-            screen.blit(text, text_pos)
+            for flight in gflights:
+                text = font.render(flight['id'], True, (255, 0, 0))
+                screen.blit(text, (flight['x'], flight['y'] + text.get_height() + 3))
+                pygame.draw.circle(screen, (255, 0, 0), (flight['x'], flight['y']), 7)
+
+            if dist_obj['distance'] <= 1000:
+                text = font.render(str(dist_obj['distance']), True, (0, 0, 0))
+
+                text_pos = (
+                    (dist_obj['p1'][0] + dist_obj['p2'][0])//2 - text.get_width() + 10,
+                    (dist_obj['p1'][1] + dist_obj['p2'][1])//2 - text.get_height() - 10
+                )
+
+                pygame.draw.line(screen, (0, 0, 255), dist_obj['p1'], dist_obj['p2'], 3)
+                pygame.draw.rect(screen, (128, 128, 128), pygame.Rect(text_pos[0] - 5, text_pos[1] - 5, text.get_width() + 10, text.get_height() + 10))
+                screen.blit(text, text_pos)
+
+        if curr_time <= 0.1:
+            curr_time = 0
+
+        timer_str = '{0:.2f}'.format(curr_time)
+        time_text = font.render(timer_str, True, (0, 0, 0))
+        if gtflights != gflights:
+            pygame.draw.rect(screen, (0, 255, 30), pygame.Rect(0, 0, 50, 30))
+        else:
+            pygame.draw.rect(screen, (128, 128, 128), pygame.Rect(0, 0, 50, 30))
+        screen.blit(time_text, (10, 10))
 
         pygame.display.flip()
